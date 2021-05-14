bfa073df859809084eebd6d97ba3f12bd0b768a6
ilaria <ilaria.secchi@avanade.com>
2021-05-14 11:38:31 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { collectExternalReferences } from '@angular/compiler';
 import { Component } from '@angular/core';
 
 @Component({
@@ -5,6 +6,44 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'App2';
-}
+export class AppComponent
+{
+  title: string = 'ciao';
+  title2: string = 'blabla';
+  nome: string = ' ';
+  // eta: number = 18;
+  // condizione: boolean = true;
+  // oggetto: {nome: string, eta: number};
+  // array = [];
+  // arrayOgg: [
+  //   {nome: string, eta: number}
+  // ];
+  //qualsiasi: any; NON USARLO CHE CREA SOLO CASINI
+  stampa: string = '';
+  testo2: string = '';
+  constructor()
+  {
+    setTimeout(() =>
+    {
+      this.nome = 'Max'
+    }, 2000);
+  }
+  //fai attenzione alle parentesi!
+    getClick(pippo: any)
+    {
+      //console.log('ciao');
+      console.log (pippo);
+    }
+
+    getText(event: any)
+    {
+      console.log (event.target.value);
+      // qua invece faccio il data binding dal component al template tramite l'interpolazione di stampa sul template
+      this.stampa=event.target.value;
+    }
+
+    getData(input: any)
+    {
+      console.log(input.value);
+    }
+  }
